d0690c44ee10b0632032dd5a9e1bbbd5a236c6ef
GosuCode <gosucode1945@gmail.com>
2023-12-12 11:35:51 +0545

    row column

--- lib/row_column.dart
+++ lib/row_column.dart
@@ -16,48 +16,65 @@ class RowColumn extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                height: 100,
-                width: 150,
-                color: Colors.orangeAccent,
+                height: 170,
+                width: 190,
+                decoration: BoxDecoration(
+                    color: Colors.orangeAccent,
+                    border: Border.all(color: Colors.blue)),
               ),
               Container(
-                height: 100,
-                width: 150,
-                color: Color.fromARGB(255, 43, 42, 40),
+                height: 170,
+                width: 190,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 43, 42, 40),
+                    border: Border.all(color: Colors.blue)),
               ),
               Container(
-                height: 100,
-                width: 150,
-                color: Color.fromARGB(255, 195, 9, 149),
+                height: 170,
+                width: 190,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 195, 9, 149),
+                    border: Border.all(color: Colors.blue)),
               ),
               Container(
-                height: 200,
-                width: 150,
-                color: Color.fromARGB(255, 17, 149, 61),
+                height: 270,
+                width: 190,
+                // color: Color.fromARGB(255, 17, 149, 61),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    border: Border.all(color: Colors.blue)),
               ),
             ],
           ),
           Column(
             children: [
               Container(
-                height: 200,
+                height: 270,
                 width: 200,
-                color: Color.fromARGB(255, 64, 80, 255),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 121, 125, 171),
+                    border: Border.all(color: Colors.blue)),
               ),
               Container(
-                height: 100,
+                height: 170,
                 width: 200,
-                color: Color.fromARGB(255, 204, 205, 215),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 204, 205, 215),
+                    border: Border.all(color: Colors.blue)),
               ),
               Container(
-                height: 100,
+                height: 170,
                 width: 200,
-                color: Color.fromARGB(255, 206, 117, 44),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 206, 117, 44),
+                    border: Border.all(color: Colors.blue)),
               ),
               Container(
-                height: 100,
+                height: 170,
                 width: 200,
-                color: Color.fromARGB(255, 250, 16, 16),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 250, 16, 16),
+                    border: Border.all(color: Colors.blue)),
               ),
             ],
           )
